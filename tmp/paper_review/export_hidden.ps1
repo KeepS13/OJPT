838686ae8428aec2fce3f8eb4d27745aa64765e5
KeepS13 <3432881734@qq.com>
2026-05-12 23:14:20 +0800

    chore: remove temporary generated files

--- tmp/paper_review/export_hidden.ps1
+++
@@ -1,8 +0,0 @@
-﻿Stop='Stop'
- = New-Object -ComObject Word.Application
-.Visible = False
-.DisplayAlerts = 0
-C:\Users\Administrator\Desktop\workspace\OJPT\.docs\论文\面向编程竞赛的SpringBoot在线算法题库系统的设计与实现论文——汪鸿韬.docx = .Documents.Open('C:\Users\Administrator\Desktop\workspace\OJPT\tmp\paper_review\paper_hidden.docx', False, True)
-C:\Users\Administrator\Desktop\workspace\OJPT\.docs\论文\面向编程竞赛的SpringBoot在线算法题库系统的设计与实现论文——汪鸿韬.docx.SaveAs2('C:\Users\Administrator\Desktop\workspace\OJPT\tmp\paper_review\paper_hidden.pdf', 17)
-C:\Users\Administrator\Desktop\workspace\OJPT\.docs\论文\面向编程竞赛的SpringBoot在线算法题库系统的设计与实现论文——汪鸿韬.docx.Close()
-.Quit()
